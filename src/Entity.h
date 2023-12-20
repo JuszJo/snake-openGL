6d39af47e8d463d96d61d376deecd0ed85b731cf
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-12-20 11:20:30 +0100

    add a base class

---
+++ src/Entity.h
@@ -0,0 +1,50 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Entity {
+    public:
+        unsigned int VAO, VBO, EBO;
+
+        void render() {
+            glBindVertexArray(VAO);
+
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        }
+
+    protected:
+        void genVertexandBuffers(unsigned int* VAO, unsigned int* VBO) {
+            glGenVertexArrays(1, VAO);
+            glGenBuffers(1, VBO);
+        }
+
+        void genElementBuffers(unsigned int* EBO) {
+            glGenBuffers(1, EBO);
+        }
+
+        void bindVAO(unsigned int VAO) {
+            glBindVertexArray(VAO);
+        }
+
+        void handleVertexBufferObject(unsigned int VBO, float* vertices, float size) {
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+        }
+
+        void handleElementBuffer(unsigned int EBO, unsigned int* indices, float size) {
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, GL_DYNAMIC_DRAW);
+        }
+
+        void handleVertexArrayObject(unsigned int VAO) {
+            
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+            glEnableVertexAttribArray(0);
+        }
+
+        void cleanupBuffers() {
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindVertexArray(0);
+        }
+};
